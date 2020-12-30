9b72a5533d16a2707364c46dd4ff95173bcaff9d
yuyatinnefeld <yuyatinnefeld@gmail.com>
2020-12-30 14:52:01 +0100

    abstract factory pattern selected for the extractor project

---
+++ project/main.py
@@ -0,0 +1,34 @@
+from extractor import *
+
+def generate(extractor: DataExtractor) -> None:
+
+    print("Generate Extractor Tools")
+    google_analytics_extractor = extractor.create_google_analytics()
+    google_search_console_extractor = extractor.create_google_search_console()
+    woocommerce_extractor = extractor.create_woocommerce()
+
+    print("## SETUP Google Analytics ##")    
+    print(f"{google_analytics_extractor.setup_google_analytics()}")
+
+    print("## SETUP Google Search Console ##")
+    print(f"{google_search_console_extractor.setup_google_search_console()}")
+
+    print("## SETUP WooCommerce ##")
+    print(f"{woocommerce_extractor.setup_woocommerce()}")
+
+
+if __name__ == "__main__":
+    """
+    The generator code can work with any concrete extractor class.
+    """
+
+    print("API Generator:")
+    generate(APIDataExtractor())
+
+    print("\n")
+    print("##########################")
+    print("\n")
+
+    print("CSV Generator:")
+    generate(CSVDataExtractor())
+
